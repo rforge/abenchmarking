243ece9bba4d5e175265256a21fbdde86bb92d90
mmarquez <mmarquez@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-10-16 20:48:39 +0000

    Project Home Page updated
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/abenchmarking@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,7 +1,3 @@
-
-<!-- This is the project specific website template -->
-<!-- It can be changed as liked or replaced by other content -->
-
 <?php
 
 $domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
@@ -33,6 +29,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <!-- get project title  -->
 <!-- own website starts here, the following may be changed as you like -->
 
+<!-- MMM
 <?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
 $contents = '';
 while (!feof($handle)) {
@@ -40,12 +37,23 @@ while (!feof($handle)) {
 }
 fclose($handle);
 echo $contents; } ?>
+MMM -->
 
-<!-- end of project description -->
+<h1>[EN] Welcome to aBenchmarking project: variable selection in DEA!</h1>
 
-<p> No content added. </p>
+<p>We propose an algorithmic method of a selection of variables in a model DEA applied to a group of decision units (DMU's). The method is based on an internal measure, which evaluates the contribution of each of the variables. </p>
 
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+<p>More information in <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>r-forge project summary page</strong></a> or in <a href="http://knuth.uca.es/moodle/course/view.php?id=74&lang=en">project page</a>.</p>
+
+<p><dl><di>References:</di><dd><a href="http://dx.doi.org/10.1590/0101-7438.2018.038.01.0031">Stepwise selection of variables in DEA using contribution loads. <i>Fernando Fernandez-Palacin, Maria Auxiliadora Lopez-Sanchez, Manuel Munoz-Marquez</i>. Pesquisa Operacional, <b>38</b>:1, pg. 31-52. 2018, DOI: 10.1590/0101-7438.2018.038.01.0031.</a></dd></dl></p>
+
+<h1>[ES] Bienvenido al proyecto aBenchmarking: selecci&oacute;n de variables en AED!</h1>
+
+<p>Proponemos un m&eacute;todo de selecci&oacute;n de variables en un modelo AED o DEA aplicado a un grupo de unidades de decisi&oacute;n o DMU. Este m&eacute;todo est&aacute; basado en una medida interna que eval&uacute;a la contribuci&oacute;n de cada variable al total.</p>
+
+<p>More information in <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>r-forge project summary page</strong></a> or in <a href="http://knuth.uca.es/moodle/course/view.php?id=74&lang=es">project page</a>.</p>
+
+<p><dl><di>Referencias:</di><dd><a href="http://dx.doi.org/10.1590/0101-7438.2018.038.01.0031">Stepwise selection of variables in DEA using contribution loads. <i>Fernando Fernandez-Palacin, Maria Auxiliadora Lopez-Sanchez, Manuel Munoz-Marquez</i>. Pesquisa Operacional, <b>38</b>:1, pg. 31-52. 2018, DOI: 10.1590/0101-7438.2018.038.01.0031.</a></dd></dl></p>
 
 </body>
 </html>
